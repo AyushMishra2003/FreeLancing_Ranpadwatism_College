30abb766316e9b6deaa42bcb1dd77c17dbc0bd96
unknown <ayushm185@gmail.com>
2024-09-24 16:17:01 +0530

    about card added

--- src/hook/useFeatureData.js
+++ src/hook/useFeatureData.js
@@ -1,37 +1,37 @@
-import image1 from '../assets/fea1.webp'
-import image2 from '../assets/fea2.webp'
-import image3 from '../assets/fea3.webp'
-import image4 from '../assets/fea4.webp'
-import image5 from '../assets/fea5.webp'
-import image6 from '../assets/fea6.webp'
-import image7 from '../assets/fea7.webp'
+import image1 from "../assets/fea1.webp";
+import image2 from "../assets/fea2.webp";
+import image3 from "../assets/fea3.webp";
+import image4 from "../assets/fea4.webp";
+import image5 from "../assets/fea5.webp";
+import image6 from "../assets/fea6.webp";
+import image7 from "../assets/fea7.webp";
 
-const useFeatureData=[
-    {
-        "img":image1,
-        "title":"Admission",
-        "link":"/comingSoon"
-    },  
-    {
-        "img":image2,
-        "title":"Acdemic",
-        "link":"/academic"
-    },
-    {
-        "img":image3,
-        "title":"Student Welfare",
-        "link":"/comingSoon"
-    },
-    {
-        "img":image4,
-        "title":"Examanination and result",
-        "link":"/result"
-    },
-    {
-        "img":image6,
-        "title":"LIbrary",
-        "link":"/library"
-    },
-]
+const useFeatureData = [
+  {
+    img: image1,
+    title: "Admission",
+    link: "/comingSoon",
+  },
+  {
+    img: image2,
+    title: "Acdemic",
+    link: "/academic",
+  },
+  {
+    img: image3,
+    title: "Student Welfare",
+    link: "/comingSoon",
+  },
+  {
+    img: image4,
+    title: "Examanination and result",
+    link: "/result",
+  },
+  {
+    img: image6,
+    title: "LIbrary",
+    link: "/library",
+  },
+];
 
-export default useFeatureData
+export default useFeatureData;
